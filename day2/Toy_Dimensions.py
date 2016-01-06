5dea5a3ec6ae1e5f1448e7957828ca08acaacd3d
bgilroy26 <bdgilroy@gmail.com>
2016-01-05 23:12:48 -0500

    cleaned up file directory

--- day2/Toy_Dimensions.py
+++
@@ -1,3 +0,0 @@
-def get_dims(in_string):
-    dim_ints = [int(dim) for dim in in_string.split('x')]
-    return tuple(dim_ints)
--- day7/run_test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 a_wire_value_evaluator.py test_input3.txt
